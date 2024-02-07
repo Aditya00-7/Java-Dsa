2720ca057837f5e817aaf56e74d9ddda0476e334
Aditya Raj <adityaraj21122018@gmail.com>
2024-02-07 23:36:25 +0530

    day 2

--- Recursion/RecursionBasics.java
+++ Recursion/RecursionBasics.java
@@ -25,10 +25,19 @@ public class RecursionBasics {
         int fn =n * fact(n-1);
         return fn;
     }
+    public  static int CalSum(int n){
+        if(n ==1){
+            return 1;
+        }
+        int Snm1 = CalSum(n-1);
+        int Sn = n+Snm1;
+        return Sn;
+    }
     public static void main(String[] args){
         int n =6;
         printDec(n);
         printInc(n);
         System.out.println(fact(n));
+        System.out.println(CalSum(n));
     }
 }
