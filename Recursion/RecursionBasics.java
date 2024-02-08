fdeee9f4d90b33183cade85ccd243fa3d6d0fb5c
Aditya Raj <adityaraj21122018@gmail.com>
2024-02-08 23:49:21 +0530

    day 3

--- Recursion/RecursionBasics.java
+++ Recursion/RecursionBasics.java
@@ -33,11 +33,35 @@ public class RecursionBasics {
         int Sn = n+Snm1;
         return Sn;
     }
+    // calculate nth terms in fibonacci
+    public static int fib(int n){
+        if (n== 0 || n ==1){
+            return n;
+        }
+        int fnm1 = fib(n-1);
+        int fnm2 = fib(n-2);
+        int fn = fnm1 +fnm2;
+        return fn;
+    }
+
+    // sorted array
+    public static boolean isSorted(int arr[], int i){
+      if (i== arr.length-1){
+          return true;
+      }
+      if(arr[i]>arr[i+1]){
+          return false;
+      }
+      return isSorted(arr, i+1);
+    }
     public static void main(String[] args){
-        int n =6;
+        int n =25;
         printDec(n);
         printInc(n);
         System.out.println(fact(n));
         System.out.println(CalSum(n));
+        System.out.println(fib(n));
+        int arr[] ={1,3,4,9,0,2,8,10};
+        System.out.println(isSorted(arr,0));
     }
 }
