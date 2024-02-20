2fc0fb98a807b9664f16c67045edd54a85c5f47f
Aditya Raj <adityaraj21122018@gmail.com>
2024-02-20 19:02:23 +0530

    day 10

--- Recursion/Pairing_problm.java
+++ Recursion/Pairing_problm.java
@@ -16,7 +16,31 @@ public class Pairing_problm {
         int totways = fnm1 +pairways;
         return totways;
     }
+
+    // print string
+    public static void printBinStrings(int n , int lastPlace,String str){
+        //base case
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+        // work to do
+        //if(lastPlace == 0){
+            // sit on the chair n
+         //   printBinStrings(n-1,0,str +"0");
+        //    printBinStrings(n-1,1,str + "1");
+       // }else {
+         //   printBinStrings(n-1,0,str+"0");
+       // }
+        // Also written as ## smple way to write it.
+        printBinStrings(n-1,0,str+"0");
+        if(lastPlace==0){
+            printBinStrings(n-1,1,str+"1");
+        }
+
+    }
     public static void main(String[] args) {
         System.out.println(friendsPairing(6));
+        printBinStrings(9,0, "");
     }
 }
