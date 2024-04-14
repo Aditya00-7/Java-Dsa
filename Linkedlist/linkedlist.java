7e673db7407866d3dabcae3f8878d9defa96e07d
Aditya Raj <adityaraj21122018@gmail.com>
2024-04-14 22:07:51 +0530

    Day 29

--- Linkedlist/linkedlist.java
+++ Linkedlist/linkedlist.java
@@ -13,9 +13,11 @@ public class linkedlist {
     }
     public static Node head;
     public static Node tail;
+    public static int size;
     public void addFirst(int data){
         //step 1 = create new node
         Node newNode = new Node(data);
+        size++;
         if (head == null){
             head = tail = newNode;
             return;
@@ -29,6 +31,7 @@ public class linkedlist {
 
     public void  addLast(int data){
         Node newNode = new Node(data);
+        size++;
         if (head == null){
             head = tail = newNode;
             return;
@@ -36,7 +39,9 @@ public class linkedlist {
         tail.next = newNode;
         tail = newNode;
     }
-    public void  print(){
+
+    // unable to call from the main function
+    public void print(){
         if (head == null){
             System.out.println("LL is empty");
             return;
@@ -46,15 +51,75 @@ public class linkedlist {
             System.out.print(temp.data+" ");
             temp = temp.next;
         }
-        System.out.println();
+        System.out.println("null");
     }
-
-
+    public void  add(int idx, int data){
+        if (idx ==0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+        while(i<idx-1){
+            temp = temp.next;
+            i++;
+        }
+        // i = idx-1; temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+    public int removeFirst(){
+        if (head == null){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size==1) {
+            int val = head.data;
+            head = tail = null;
+            size =0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+    public int removeLast(){
+        if (size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size==1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        // prev : i = size-2
+        Node prev = head;
+        for (int i =0;i<size-2;i++){
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
     public static void main(String args[]){
         LinkedList Ll = new LinkedList ();
         Ll.addFirst(2);
         Ll.addFirst(1);
         Ll.addLast(3);
         Ll.addLast(4);
+        // Ll.print();
+        Ll.add(2,9);
+        System.out.println(Ll);
+        System.out.println(Ll.size());
+        Ll.removeFirst();
+        System.out.println(Ll);
+        Ll.removeLast();
+        System.out.println(Ll);
+
     }
 }
