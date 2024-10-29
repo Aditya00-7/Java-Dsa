d8cc532accb1c92528b085c0871afdb753ec5c8f
Aditya Raj <adityaraj21122018@gmail.com>
2024-10-30 00:27:18 +0530

    tries done

--- Tries/wordbreak.java
+++ Tries/wordbreak.java
@@ -1,10 +1,9 @@
 package Tries;
 
-public class classroom {
+public class wordbreak {
     static class Node {
         Node children[] = new Node[26];
         boolean eow = false;
-
         Node(){
             for (int i=0;i<26;i++){
                 children[i]=null;
@@ -12,6 +11,7 @@ public class classroom {
         }
     }
     public static Node root = new Node();
+
     public static  void insert(String word){
         Node curr = root;
         for (int level =0;level<word.length();level++){
@@ -33,15 +33,27 @@ public class classroom {
             }
             curr = curr.children[idx];
         }
-       return curr.eow= true;
+        return curr.eow= true;
     }
-    public static void main(String[] args) {
-        String words[]={"the","a", "there","their","any","thee"};
-        for (int i =0;i< words.length;i++){
-         insert(words[i]);
+    public static boolean wordbreak(String key){ // o(l)
+        if (key.length()==0){
+            return true;
         }
+        for (int i=1;i<=key.length();i++){
+           if(search(key.substring(0,i)) &&
+            wordbreak(key.substring(i))){
+               return true;
+            }
+        }
+        return false;
+    }
 
-        System.out.println(search("thee"));
-        System.out.println(search("thor"));
+    public static void main(String[] args) {
+        String arr[]={"i","like","sam","samsung","mobile","ice"};
+        for (int i=0;i< arr.length;i++){
+            insert(arr[i]);
+        }
+        String key="ilikesamsung";
+        System.out.println(wordbreak(key));
     }
 }
