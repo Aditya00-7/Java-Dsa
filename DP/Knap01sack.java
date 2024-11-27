ac5a289c85608a6a77f03d7c621eeb8040d650b0
Aditya Raj <adityaraj21122018@gmail.com>
2024-11-27 22:42:21 +0530

    update

--- DP/Knap01sack.java
+++ DP/Knap01sack.java
@@ -2,18 +2,25 @@ package DP;
 
 public class Knap01sack {
 
-    public static int knapsack(int  val[],int wt[],int W,int n){
+    public static int knapsack(int  val[],int wt[],int W,int n,int dp[][]){
         if (W==0||n==0){
             return 0;
         }
+        if (dp[n][W]!=-1){
+            return dp[n][W];
+        }
         if (wt[n-1]<=W){//valid
             //include
-            int ans1  = val[n-1]+knapsack(val,wt,W-wt[n-1],n-1);
+            int ans1  = val[n-1]+knapsack(val,wt,W-wt[n-1],n-1,dp);
             //exclude
-            int ans2 = knapsack(val, wt, W, n-1);
-            return Math.max(ans1,ans2);
+            int ans2 = knapsack(val, wt, W, n-1,dp);
+            dp[n][W] = Math.max(ans1,ans2);
+            return dp[n][W];
+           //  return Math.max(ans1,ans2); for recursion
         }else {
-            return knapsack(val,wt,W,n-1);
+            dp[n][W]= knapsack(val, wt, W, n-1, dp);
+            return dp[n][W];
+             // return knapsack(val,wt,W,n-1,dp);for recursion
         }
     }
 
@@ -22,6 +29,14 @@ public class Knap01sack {
         int wt[] = {2,5,1,3,4};
         int W =7;
 
-        System.out.println(knapsack(val,wt,W, val.length));
+        // Memoizataion
+        int dp[][] = new int [val.length+1][W+1];
+         for (int i=0;i< dp.length;i++){
+             for (int j=0;j<dp[0].length;j++){
+                 dp[i][j]=-1;
+             }
+         }
+
+        System.out.println(knapsack(val,wt,W, val.length,dp));
     }
 }
